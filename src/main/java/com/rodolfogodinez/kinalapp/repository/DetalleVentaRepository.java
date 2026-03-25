9be50e52513b9c22cbe2b3f2b2ef325f119607e1
rgodinez-2022107 <rgodinez-2022107@kinal.edu.gt>
2026-03-25 09:06:03 -0600

    feat: "Se agregó el repositorio DetalleVentaRepository"

---
+++ src/main/java/com/rodolfogodinez/kinalapp/repository/DetalleVentaRepository.java
@@ -0,0 +1,11 @@
+package com.rodolfogodinez.kinalapp.repository;
+
+import java.util.List;
+
+import com.rodolfogodinez.kinalapp.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta,Integer> {
+
+
+}
