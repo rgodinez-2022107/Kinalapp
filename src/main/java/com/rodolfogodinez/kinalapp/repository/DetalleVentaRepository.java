30656dc346745848712759e5342ccc5fc592cdce
rgodinez-2022107 <rgodinez-2022107@kinal.edu.gt>
2026-03-27 22:54:11 -0600

    fix: "Cambiar tipo de ID de Integer a Long en DetalleVentaRepository"

--- src/main/java/com/rodolfogodinez/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/rodolfogodinez/kinalapp/repository/DetalleVentaRepository.java
@@ -1,12 +1,13 @@
 package com.rodolfogodinez.kinalapp.repository;
 
+import com.rodolfogodinez.kinalapp.entity.DetalleVenta;
+import com.rodolfogodinez.kinalapp.entity.Ventas;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-import com.rodolfogodinez.kinalapp.entity.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
+public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
 
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta,Integer> {
+    List<DetalleVenta> findByVenta(Ventas venta);
 
-    List<DetalleVenta> findByVenta_CodigoVenta(Integer codigoVenta);
-
+    List<DetalleVenta> findByVenta_CodigoVenta(Long codigoVenta);
 }
